1e8686f1bd57152dcc64a81a6f28a0c0d9b565b4
Khaled Hussein <khaled.hussein@gmail.com>
2012-10-11 21:39:36 -0700

    Reorganizes the payment processor settings.

--- config/initializers/amazon_flexible_payments.rb
+++ config/initializers/amazon_flexible_payments.rb
@@ -1,3 +1,3 @@
-AmazonFlexPay.access_key = Settings.amazon_access_key
-AmazonFlexPay.secret_key = Settings.amazon_secret_key
+AmazonFlexPay.access_key = Settings.payment_processor.amazon.access_key
+AmazonFlexPay.secret_key = Settings.payment_processor.amazon.secret_key
 AmazonFlexPay.go_live! if Rails.env.production?
