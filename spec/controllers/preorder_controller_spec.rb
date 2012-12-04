caf0f232110562d0aa5e0b49c2ee3957c3426346
Marin Usalj <mneorr@gmail.com>
2012-12-04 12:34:25 -0800

    tested order controller completely

--- spec/controllers/preorder_controller_spec.rb
+++ spec/controllers/preorder_controller_spec.rb
@@ -44,6 +44,43 @@ describe PreorderController do
 
   end
 
+  describe "#postfill" do
+    fixtures :orders
+    
+    before do
+      @uuid = 'ec781fa2-c5e6-4af9-8049-4dee15a85296'
+      Order.stub(:postfill!).and_return orders(:one)
+    end
+
+    it "checks for the caller refernece existance" do
+      expect { get :postfill 
+               assigns(:order).should be_nil }.to_not raise_error
+      
+    end
+
+    it "postfills the order" do
+      get :postfill, callerReference: @uuid
+      assigns(:order).uuid.should == @uuid
+    end
+
+    it "redirects to root_url if user cancelled the preorder before clicking 'Confirm' on Amazon Payments" do
+      get :postfill, callerReference: @uuid, status: 'A'
+      response.should redirect_to root_url
+    end
+
+    it "redirects to root url if order is not present" do
+      Order.should_receive(:postfill!).and_return nil
+      get :postfill, callerReference: @uuid, status: 'B'
+      response.should redirect_to root_url
+    end
+
+    it "redirects to #share if everything's ok" do
+      get :postfill, callerReference: @uuid, status: 'B'
+      response.should redirect_to action: 'share', uuid: assigns(:order).uuid
+    end
+
+  end
+
   describe "#share" do
 
     it "finds the order by uuid" do
