cbdbc4959c0202fe8a0504725420cce2780259d5
Jarred Sumner <jarred@jarredsumner.com>
2013-04-10 12:14:24 -0700

    Generate UUID on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email
   has_many :orders
+  before_validate :generate_uuid!, :on => :create
+  
+  def generate_uuid!
+    self.uuid = SecureRandom.hex(9)
+  end
 end
