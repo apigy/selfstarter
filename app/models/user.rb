ef05e3c6db5f89d3892df21e31fc5f24d1e9ea48
Guy Maliar <gmaliar@gmail.com>
2013-04-21 01:14:41 +0300

    Update user.rb
    Doesn't belong to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email
   has_many :orders
-  before_validation :generate_uuid!, :on => :create
-  validates_presence_of :uuid
-  self.primary_key = 'uuid'
-  
-  def generate_uuid!
-    begin
-      self.uuid = SecureRandom.hex(16)
-    end while Order.find_by_uuid(self.uuid).present?
-    end
 end
