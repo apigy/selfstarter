12c0bcc08b2fbb40632aff6622b37be9bb30627c
Jason Wan <jwan823@gmail.com>
2013-12-10 22:45:05 +0000

    FactoryGirl is in, Fixtures are out. However there are still failing tests (separate task)

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,5 +2,22 @@
 
 FactoryGirl.define do
   factory :order do
+    user
+    name { Faker::Name.first_name }
+    price { (Faker::Number.number(3).to_f)/100 }
+    number 1
+    
+    factory :authenticated_order do
+      addressLine1 { Faker::Address.street_address }
+      addressLine2 { Faker::Address.secondary_address }
+      city { Faker::Address.city }
+      state { Faker::Address.state }
+      zip { Faker::Address.zip_code }
+      phoneNumber { Faker::PhoneNumber.cell_phone }
+      country { Faker::Address.country }
+      status "IN PROGRESS"
+      token 128736127863
+      expiration { Date.parse(Time.now + 99999) }
+    end
   end
 end
