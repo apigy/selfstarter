14e3fc3ae64120979026100a1f794cbf511457c4
Tyler Willingham <tylerwillingham@gmail.com>
2014-11-20 02:06:53 -0600

    Removed mass assignment tests from Order spec
    Testing mass assignment within the model is no longer necessary after
    the introduction of strong_paramters in Rails 4.0

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,15 +2,6 @@ describe Order do
 
   context "attributes" do
 
-    [:address_one, :address_two, :city, :country, :number, :state, :status,
-      :token, :transaction_id, :zip, :shipping, :tracking_number, :name,
-      :price, :phone, :expiration
-      ].each do |property|
-        it { should allow_mass_assignment_of property }
-      end
-
-      it { should_not allow_mass_assignment_of :uuid }
-
       it "generates UUID before validation on_create" do
         @order = Order.new
         @order.valid?
