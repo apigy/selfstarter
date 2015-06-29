dfe95628dd2b7877588cf87141cb47721a4b3785
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-29 21:53:58 +0200

    Say thanks instead of using Amazon

--- app/controllers/preorder_controller.rb
+++ app/controllers/preorder_controller.rb
@@ -19,30 +19,17 @@ class PreorderController < ApplicationController
       price = Settings.price
     end
 
-    @order = Order.prefill!(:name => Settings.product_name, :price => price, :user_id => @user.id, :payment_option => payment_option)
+    @order = Order.prefill!(
+      :name => Settings.product_name,
+      :price => price,
+      :user_id => @user.id,
+      :payment_option => payment_option,
+      :email => params[:email])
 
-    # This is where all the magic happens. We create a multi-use token with Amazon, letting us charge the user's Amazon account
-    # Then, if they confirm the payment, Amazon POSTs us their shipping details and phone number
-    # From there, we save it, and voila, we got ourselves a preorder!
-    port = Rails.env.production? ? "" : ":3000"
-    callback_url = "#{request.scheme}://#{request.host}#{port}/preorder/postfill"
-    redirect_to AmazonFlexPay.multi_use_pipeline(@order.uuid, callback_url,
-                                                 :transaction_amount => price,
-                                                 :global_amount_limit => price + Settings.charge_limit,
-                                                 :collect_shipping_address => "True",
-                                                 :payment_reason => Settings.payment_description)
+    redirect_to action: :postfill
   end
 
   def postfill
-    unless params[:callerReference].blank?
-      @order = Order.postfill!(params)
-    end
-    # "A" means the user cancelled the preorder before clicking "Confirm" on Amazon Payments.
-    if params['status'] != 'A' && @order.present?
-      redirect_to :action => :share, :uuid => @order.uuid
-    else
-      redirect_to root_url
-    end
   end
 
   def share
--- app/models/order.rb
+++ app/models/order.rb
@@ -72,7 +72,7 @@ class Order < ActiveRecord::Base
       PaymentOption.joins(:orders).where("token != ? OR token != ?", "", nil).pluck('sum(amount)')[0].to_f
     else
       Order.completed.sum(:price).to_f
-    end 
+    end
   end
 
   validates_presence_of :name, :price, :user_id
