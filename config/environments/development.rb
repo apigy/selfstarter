fefcb741a79db0f48329537b967512ceec7106c7
Denis Defreyne <denis.defreyne@stoneship.org>
2015-06-29 20:50:27 +0200

    Disable jquery, because

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Selfstarter::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  config.assets.precompile += %w( application.js application.css )
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
