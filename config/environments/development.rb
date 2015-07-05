0d31c6f36bea246821c916cb8ba6955c49cb8e4d
Denis Defreyne <denis.defreyne@stoneship.org>
2015-07-05 15:42:14 +0200

    Send order confirmation e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,9 @@ Selfstarter::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'denis@stoneship.org'}
 end
