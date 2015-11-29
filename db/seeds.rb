f7d045d34eec0a8baa21b9b31b43bf6c88fe584f
Charlie Palanzo <palanzo@yahoo.com>
2015-11-29 13:32:37 -0500

    test 10000 option

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+PaymentOption.destroy_all;
+
 PaymentOption.create(
     [
         {
