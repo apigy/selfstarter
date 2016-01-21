760c07cd1b5a3da70c8504f5d7b5ebf58abf93ee
Micael Nussbaumer <micaelnussbaumer@gmail.com>
2016-01-21 22:43:32 +0000

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Selfstarter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.log_level = :info
   #UPDATED for new rails version
   config.eager_load = true
   
@@ -12,7 +12,7 @@ Selfstarter::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
